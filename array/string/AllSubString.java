7197ac87464f52b38e4dc555efdb0e54189749d7
Dhruv yadav <ymulayamyadav99@gmail.com>
2026-07-09 14:31:32 +0530

    substring

---
+++ array/string/AllSubString.java
@@ -0,0 +1,13 @@
+package array.string;
+
+public class AllSubString {
+    public static void main (String [] arg){
+        String s= "Dhruv yadav";
+        for(int i =0; i<s.length(); i++){
+            for(int j =i; j<s.length();j++){
+                System.out.println(s.substring(i,j));
+            }
+        }
+    }
+    
+}
---
+++ array/string/SubString1.java
@@ -0,0 +1,10 @@
+package array.string;
+
+public class SubString1 {
+    public static void main (String [] args){
+        String s ="Mulayam Singh Yadav";
+        System.out.println(s.substring(0,6));
+        System.out.println(s.substring(7));
+        
+    }
+}
