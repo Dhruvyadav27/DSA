394dc6eca66efdc65a635ccacfed28bb0f472eac
Dhruv yadav <ymulayamyadav99@gmail.com>
2026-07-11 00:00:18 +0530

    substring

--- array/string/AllSubString.java
+++ array/string/AllSubString.java
@@ -3,8 +3,8 @@ package array.string;
 public class AllSubString {
     public static void main (String [] arg){
         String s= "Dhruv yadav";
-        for(int i =0; i<s.length(); i++){
-            for(int j =i; j<s.length();j++){
+        for(int i =0; i<=s.length(); i++){
+            for(int j =i; j<=s.length();j++){
                 System.out.println(s.substring(i,j));
             }
         }
